02b11de42be52904526027b6e9a14c6d9911e0c9
Hitray <75577433+Hitray@users.noreply.github.com>
2022-08-09 22:12:29 +0500

    Update WordsChecker.java

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class WordsChecker {
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordsChecker {
+    private final Set<String> wordsHashSet = new HashSet<>();
+
+    public WordsChecker(String text) {
+        Collections.addAll(wordsHashSet, text.split("\\P{IsAlphabetic}+"));
+    }
+
+    public boolean hasWord(String s) {
+        return wordsHashSet.contains(s);
+    }
 }
